7102d5526942243a423e02f88dcb630ed2b0af16
FannyC13 <94389445+FannyC13@users.noreply.github.com>
2023-10-21 20:29:05 +0200

    Ajout de Plot

--- Project_DataViz/Project.py
+++ Project_DataViz/Project.py
@@ -9,7 +9,10 @@ from streamlit_pandas_profiling import st_profile_report
 from ydata_profiling import ProfileReport
 import base64
 
-st.header("Welcome to the Data Visualisation Project")
+st.header("Bienvenue sur notre projet de visualisation de données :smile:")
 
 df = pd.read_csv("C:\\Users\\fchan\\OneDrive - Efrei\\M1\\Semestre 7\\Big Data Applications I\\Data Vizualisation\\Streamlit\\dataset.csv", index_col = [0],delimiter=';')
-" This project aims to analyze the integration of Masters programs in France. In order to carry out this analysis, we will be using various plotting methods."
+"Ce projet vise à analyser l'intégration des masters en France. Pour mener à bien cette analyse, nous utiliserons différentes méthodes de représentation graphique"
+
+"Cette page est la page principale du projet, vous trouverez dans 'Exploratory Analysis' toutes les informations concernant le jeu de données ainsi que les détails du profiling et du data cleaning"
+"La page 'Plotting' contient les différents plot qui nous permettront d'effectuer une analyse approfondie de l'ensemble des données. Cette page est divisée en différentes sections pour une meilleure vue d'ensemble de l'analyse"
--- Project_DataViz/pages/Plotting.py
+++ Project_DataViz/pages/Plotting.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import numpy as np
 import streamlit as st
+import json
+import folium
+from streamlit_folium import folium_static
 
 
 st.header("Plotting Data")
@@ -53,6 +56,8 @@ elif choix == "Analyse Type Emploi":
 
     st.subheader("Analyse Emploi")
 
+    #-------------------------------------------------------------Evolution Du Nombre de cadre par discipline------------------------------------------------------------------#
+
     Bar1, Line, Bar2 = st.tabs(["Bar Plot", "Line Chart", "Bar Empilées"])
 
     st.subheader("Plot Evolution Du Nombre de cadre par discipline")
@@ -61,7 +66,7 @@ elif choix == "Analyse Type Emploi":
     selected_annees = st.multiselect("Sélectionnez les années (cadres)", annees,annees)
     
     df_cadres_par_discipline_par_annee = df[df["annee"].isin(selected_annees)].groupby(["annee", "discipline"])["emplois_cadre"].sum().reset_index()
-    #df_cadres_par_discipline_par_annee = df[df["annee"].isin(selected_annees)].groupby("discipline")["emplois_cadre"].sum().reset_index()
+    
     plot_choice = st.radio("Select a Plot", ["Bar Chart 1", "Line Chart", "Bar Chart 2"])
 
     if plot_choice == "Bar Chart 1":
@@ -76,3 +81,93 @@ elif choix == "Analyse Type Emploi":
         fig = px.bar(df_cadres_par_discipline_par_annee, x="annee", y="emplois_cadre", color="discipline", title="Nombre d'emplois cadre par discipline")
         st.plotly_chart(fig)
 
+    #------------------------------------------------------------- Taux d'insertion par discipline------------------------------------------------------------------#
+
+    st.subheader("Plot Du Taux d'insertion par discipline")
+
+    selected_annees = st.multiselect("Sélectionnez les années", annees,annees, key="taux_insertion_par_discipline")
+
+    df_insertion_par_disicpline_par_annee = df[df["annee"].isin(selected_annees)].groupby("discipline")["taux_dinsertion"].mean().reset_index()
+
+    fig = px.bar(df_insertion_par_disicpline_par_annee, x="discipline", y="taux_dinsertion", title="Taux d'insertion par discipline")
+    
+    for i in range(len(df_insertion_par_disicpline_par_annee)):
+        discipline = df_insertion_par_disicpline_par_annee.loc[i, "discipline"]
+        taux_insertion = df_insertion_par_disicpline_par_annee.loc[i, "taux_dinsertion"]
+        taux_insertion_percentage = f"{taux_insertion:.2f}%" 
+        fig.add_annotation(
+            x=discipline,
+            y=taux_insertion,
+            text=f"{taux_insertion_percentage}", 
+            showarrow=False,
+            font=dict(size=9),
+            yshift=12
+        )
+
+    st.plotly_chart(fig)
+
+    #-------------------------------------------------------------Taux d'insertion par academie------------------------------------------------------------------#
+
+    st.subheader("Plot Du Taux d'insertion par academie")
+
+    selected_annees = st.multiselect("Sélectionnez les années (academie)", annees,annees, key="taux_insertion_par_academie")
+    df_insertion_par_academie_par_annee = df[df["annee"].isin(selected_annees)].groupby("academie")["taux_dinsertion"].mean().reset_index()
+    
+    plot_choice_2 = st.radio("Select a Plot", ["Map", "Bar Chart"])
+
+    if plot_choice_2 == "Bar Chart":
+        fig = px.bar(df_insertion_par_academie_par_annee, x="academie", y="taux_dinsertion", title="Taux d'insertion par academie")
+
+        for i in range(len(df_insertion_par_academie_par_annee)):
+            academie = df_insertion_par_academie_par_annee.loc[i, "academie"]
+            taux_insertion = df_insertion_par_academie_par_annee.loc[i, "taux_dinsertion"]
+            taux_insertion_percentage = f"{taux_insertion:.1f}%" 
+            fig.add_annotation(
+                x=academie,
+                y=taux_insertion,
+                text=f"{taux_insertion_percentage} ", 
+                showarrow=False,
+                font=dict(size=9),
+                yshift=13
+            )
+
+        st.plotly_chart(fig)
+   
+    if plot_choice_2 == "Map":
+    
+        with open('academie.geojson') as f:
+            geojson_data = json.load(f)
+
+        m = folium.Map(location=[48.8566, 2.3522], zoom_start=5) #France
+
+        for feature in geojson_data['features']:
+            academie = feature['properties']['name']
+            taux_dinsertion = df_insertion_par_academie_par_annee[df_insertion_par_academie_par_annee['academie'] == academie]['taux_dinsertion'].mean()
+            feature['properties']['taux_dinsertion'] = taux_dinsertion
+
+            latitude, longitude = feature['properties']['geo_point_2d']
+            tooltip = f'{academie}<br>Taux d\'Insertion : {taux_dinsertion:.2f}%'
+            folium.Marker(
+                location=[latitude, longitude],
+                tooltip=tooltip,
+                icon=folium.DivIcon(
+                    icon_size=(20, 20), 
+                    icon_anchor=(10, 10),
+                    html=f'<i class="fa fa-map-marker fa-2x" style="color: #61677A;"></i>' 
+                )
+            ).add_to(m)
+
+
+        folium.Choropleth(
+            geo_data=geojson_data,
+            data=df_insertion_par_academie_par_annee,
+            columns=['academie', 'taux_dinsertion'],
+            key_on='feature.properties.name',
+            fill_color='Pastel1',
+            fill_opacity=0.5,
+            line_opacity=0.2,
+            legend_name='Taux d\'Insertion (%)'
+        ).add_to(m)
+
+        st.title('Carte Choroplète des Taux d\'Insertions par Académie')
+        folium_static(m)
